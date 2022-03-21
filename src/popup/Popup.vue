a77f73cb3a711730ab6d9f5d6d15fc74b516d7ae
senpp <phuongthephung@gmail.com>
2022-03-21 10:05:20 +0700

    feat: added youtube downloader func

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,8 +1,21 @@
 <template>
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
     <Logo />
-    <h3>Fugon11's Chrome Extension version</h3>
+    <h3>Fugon11's Chrome Extension</h3>
+
+    <div class="mt-4">
+      <p class="mb-2">Youtube Downloader</p>
+      <input v-model="url" type="text" placeholder="v?=" class="focus:outline-none border-gray-300 rounded px-3 py-1.5 border-1 mr-2" />
+      <button class="bg-violet-900 text-white rounded px-3 py-1.5" @click="download()">Download</button>
+    </div>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const url = ref('')
+
+// eslint-disable-next-line space-before-function-paren
+const download = async () => {
+  window.open(`https://fugon11-youtube-downloader.herokuapp.com/watch?v=${url.value}`)
+}
+</script>
