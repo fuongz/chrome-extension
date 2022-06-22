08ba21980615db1761156052a544c94f92b4065f
fugon11 <phuongthephung@gmail.com>
2022-06-22 23:31:15 +0700

    chore: bump deps

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,33 +1,5 @@
-<template>
-  <main class="w-[300px] px-4 py-3 text-gray-700">
-    <div>
-      <input v-model="url" type="text" placeholder="v?=" class="focus:outline-none border-gray-300 rounded px-3 py-1.5 border-1 mr-2" />
-      <button class="bg-violet-900 text-white rounded px-3 py-1.5" @click="download()">Download</button>
-    </div>
-
-    <div class="mt-4 border-t pt-4 border-gray-150">
-      <div v-if="data && data.length > 0">
-        <table class="w-full table">
-          <tr v-for="item in data" :key="item.key">
-            <td class="text-left">
-              {{ item.key }}
-            </td>
-            <td class="text-left">
-              <span class="font-semibold text-emerald-600">{{ item.usd }}</span>
-            </td>
-          </tr>
-        </table>
-      </div>
-
-      <div v-else>
-        <div class="text-center text-gray-600">No data.</div>
-      </div>
-    </div>
-  </main>
-</template>
-
 <script setup lang="ts">
-type Coin = {
+interface Coin {
   key: string
   usd: string
 }
@@ -54,8 +26,35 @@ onMounted(async () => {
   }
 })
 
-// eslint-disable-next-line space-before-function-paren
 const download = async () => {
   window.open(`https://fugon11-youtube-downloader.herokuapp.com/watch?v=${url.value}`)
 }
 </script>
+
+<template>
+  <main class="w-[300px] px-4 py-3 text-gray-700">
+    <div>
+      <input v-model="url" type="text" placeholder="v?=" class="focus:outline-none border-gray-300 rounded px-3 py-1.5 border-1 mr-2">
+      <button class="bg-violet-900 text-white rounded px-3 py-1.5" @click="download()">Download</button>
+    </div>
+
+    <div class="mt-4 border-t pt-4 border-gray-150">
+      <div v-if="data && data.length > 0">
+        <table class="w-full table">
+          <tr v-for="item in data" :key="item.key">
+            <td class="text-left">
+              {{ item.key }}
+            </td>
+            <td class="text-left">
+              <span class="font-semibold text-emerald-600">{{ item.usd }}</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div v-else>
+        <div class="text-center text-gray-600">No data.</div>
+      </div>
+    </div>
+  </main>
+</template>
